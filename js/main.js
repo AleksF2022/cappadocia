909cddf801c2e0bda506e4db962abd0df18dc8e9
AleksF2022 <aleksfomin52@gmail.com>
2024-01-28 07:35:42 +0200

    trip completed

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ $(function () {
         nextArrow: '<button type="button" class="slick-next slick-btn"> <img src="images/arrow-next.svg" alt=""></button>',
 
     });
+    $('.trip__slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next slick-btn"> <img src="images/arrow-next.svg" alt=""></button>',
+
+    });
 });
