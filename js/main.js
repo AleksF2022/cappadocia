ab9c135e90f66d335b0496ddd86c435d088a384a
AleksF2022 <aleksfomin52@gmail.com>
2024-01-28 20:00:57 +0200

    price

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function () {
     $('.fairy__slider').slick({
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next slick-btn"> <img src="images/arrow-next.svg" alt=""></button>',
 
     });
     $('.trip__slider').slick({
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next slick-btn"> <img src="images/arrow-next.svg" alt=""></button>',
